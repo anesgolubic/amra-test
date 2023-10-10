95fdeb09e4f745c31363d62971343a5ae5a3a5e3
anesgolubic <anes.golubic@gmail.com>
2023-10-10 21:09:59 +0200

    new css

--- Home.py
+++ Home.py
@@ -11,8 +11,8 @@ from datetime import date, timedelta
 import numpy as np
 
 # LINK TO THE CSS FILE
-#with open('style.css')as f:
-    #st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
+with open('style.css')as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html = True)
 
 """
 # Amra test
