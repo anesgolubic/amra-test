1f9c29166b4e57c3acf9a7940b8a92b95cbbb475
anesgolubic <anes.golubic@gmail.com>
2023-10-10 20:55:00 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -32,7 +32,7 @@ df = services.query('Servis == "'+str(kategorija)+'" & Users == "'+str(users)+'"
 st.write(df)
 
 
-dokumenti = pd.read_excel('Mapping of services.xlsx',sheet_name='Lista potrebnih dokumenata', skiprows = range(1, 9))
+dokumenti = pd.read_excel('Mapping of services.xlsx',sheet_name='Lista potrebnih dokumenata', skiprows = range(1, 9), header = 9)
 
 """
 ## Lista potrebnih dokumenata
