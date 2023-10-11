ccd3ef066abf72327829e4055ceaf8c447dbaf21
anesgolubic <anes.golubic@gmail.com>
2023-10-11 08:47:24 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -44,31 +44,28 @@ for index, row in df.iterrows():
     #st.markdown('<p style="margin:0;display:inline;float:left" class="blog-label">'+str(row['Users'])+'</p>', unsafe_allow_html=True)
     st.markdown('<h3>'+str(row['Name'])+'</h3>', unsafe_allow_html=True)
     st.markdown('<p>'+str(row['Description'])+'</p>', unsafe_allow_html=True)
-    st.divider()
-    st.write('Government Agency/Organization')
-    st.markdown('<h4>'+str(row['Government Agency/Organization'])+'</h3>', unsafe_allow_html=True)
+    #st.divider()
+    #st.write('Government Agency/Organization')
+    #st.markdown('<h4>'+str(row['Government Agency/Organization'])+'</h3>', unsafe_allow_html=True)
 
-modal = Modal(key="Demo Key", title="Demo Modal")
-open_modal = st.button("Open")
-if open_modal:
-    modal.open()
 
-if modal.is_open():
-    with modal.container():
-        st.write("Text goes here")
+    modal = Modal(key="Demo Key", title="Government Agency/Organization")
+    open_modal = st.button("Government Agency/Organization")
+    if open_modal:
+        modal.open()
 
-        html_string = '''
-        <h1>HTML string in RED</h1>
+    if modal.is_open():
+        with modal.container():
+            st.write("Text goes here")
 
-        <script language="javascript">
-          document.querySelector("h1").style.color = "red";
-        </script>
-        '''
-        components.html(html_string)
-
-        st.write("Some fancy text")
-        value = st.checkbox("Check me")
-        st.write(f"Checkbox checked: {value}")
+            html_string = '''
+            <h3>Ministarstvo za rad, socijalnu politiku, raseljena lica i izbjeglice</h3>
+            <p>Reisa Džemaludina Čauševića 1</p>
+            <p>033/723-635</p>
+            <p>mirsada@kcsr.ba</p>
+            <a href="https://mrsri.ks.gov.ba/">Website</a>
+            '''
+            components.html(html_string)
 
 dokumenti = pd.read_excel('Mapping of services.xlsx',sheet_name='Lista potrebnih dokumenata', skiprows = range(1, 8), header = 1)
 
