b1ab08efaea119f59095487362db8d265f244ac6
anesgolubic <anes.golubic@gmail.com>
2023-10-11 08:37:52 +0200

    modal

--- Home.py
+++ Home.py
@@ -7,7 +7,8 @@ st.set_page_config(
 )
 
 import pandas as pd
-import st_pop_up_component as sp
+from streamlit_modal import Modal
+import streamlit.components.v1 as components
 from datetime import date, timedelta
 import numpy as np
 
@@ -47,7 +48,27 @@ for index, row in df.iterrows():
     st.write('Government Agency/Organization')
     st.markdown('<h4>'+str(row['Government Agency/Organization'])+'</h3>', unsafe_allow_html=True)
 
+modal = Modal("Demo Modal")
+open_modal = st.button("Open")
+if open_modal:
+    modal.open()
 
+if modal.is_open():
+    with modal.container():
+        st.write("Text goes here")
+
+        html_string = '''
+        <h1>HTML string in RED</h1>
+
+        <script language="javascript">
+          document.querySelector("h1").style.color = "red";
+        </script>
+        '''
+        components.html(html_string)
+
+        st.write("Some fancy text")
+        value = st.checkbox("Check me")
+        st.write(f"Checkbox checked: {value}")
 
 dokumenti = pd.read_excel('Mapping of services.xlsx',sheet_name='Lista potrebnih dokumenata', skiprows = range(1, 8), header = 1)
 
