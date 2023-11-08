23fa9ef195d72c4419624993162045c96a2840cf
anesgolubic <anes.golubic@gmail.com>
2023-11-08 23:23:01 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -73,7 +73,7 @@ st.write(usluge)
 #Filteri
 col1, col2 = st.columns(2)
 with col1:
-    zivotna_dob = st.selectbox('Odaberite životnu dob:',zd,'Sve')
+    zivotna_dob = st.selectbox('Odaberite životnu dob:',options=zd,placeholder='Izaberi neku od opcija', index=-1)
 
 with col2:
-    usluga = st.selectbox('Odaberite Tip usluge/prava/benefita:',usluge,'Sve')
+    usluga = st.selectbox('Odaberite Tip usluge/prava/benefita:',options=usluge,placeholder='Izaberi neku od opcija', index=-1)
